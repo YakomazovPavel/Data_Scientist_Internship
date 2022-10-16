884e74c484b2ac20f5567133c11717da6c2ae609
михан <>
2022-10-16 20:56:49 +0300

    update

--- main.py
+++ main.py
@@ -9,120 +9,69 @@ def sumchar(item):
     return out
 
 
-def main():
-    """Функции func1 и func2 возвращают кортеж, состоящий их кортежей типа (Номер_группы, число покупателей)
+"""
+    Функции func1 и func2 возвращают список, состоящий их кортежей типа (Номер_группы, число покупателей)
 
-        func1 выполнгяется для n_customers первых строк,
-        func2 выполнятестся для n_customers от указанного id (n_first_id)
+    func1 выполнгяется для n_customers первых строк,
+    func2 выполнятестся для n_customers от указанного id (n_first_id)
 
-        Лирическое отступление:
-        Была созданана тестовая БД. Не зная структуры основной бд, я мог не учесть некоторые особенности
-        например: мои ID это значения INTEGER. в то время как в настоящей базе они могли быть VARCHAR и тогда
-         ID 00001, 000001, 0000001 - это были бы три разных ID =), В то время как у меня все они равны 1.
-         Так же непонятно упорядочены ли записи по ID, нужно ли применять сортировку перед тем как достать записи из БД?
-         В общем все эти вопросы я было задал перед тем как приступать к работе, но так как спращивать не у кого
-    """
-    def func1(n_customers):
-        with sqlite3.connect('db/database.db') as db:
-            cursor = db.cursor()
-
-            query = """SELECT id FROM customers LIMIT %(n_customers)d""" % {'n_customers': n_customers}
-            ids = list(cursor.execute(query))
-            for i in range(0, len(ids)):
-                item = list(ids[i])
-                item.append(sumchar(item[0]))
-                ids[i] = tuple(item)
-
-            query = """
-                   DROP TABLE IF EXISTS temp;
-                   """
-            cursor.execute(query)
-
-            query = """
-                    CREATE TABLE temp (
-                    ID INTEGER UNIQUE NOT NULL,
-                    NumGroup INTEGER NOT NULL,
-                    PRIMARY KEY(ID))
-                    """
-            cursor.execute(query)
-
-            query = """
-                    INSERT INTO temp (ID, NumGroup)
-                    VALUES (?,?);
-                    """
-            cursor.executemany(query, ids)
-
-            query = """
-                    SELECT temp.NumGroup, COUNT(customers.ID)
-                    FROM customers
-                    LEFT JOIN temp
-                    ON customers.ID = temp.ID
-                    WHERE temp.NumGroup <> 'None'
-                    GROUP BY NumGroup
-                    """
-
-            out = tuple(cursor.execute(query))
-
-            query = """
-                   DROP TABLE IF EXISTS temp;
-                   """
-
-            cursor.execute(query)
-
-            return out
-
-    def func2(n_first_id, n_customers):
-        with sqlite3.connect('db/database.db') as db:
-            cursor = db.cursor()
-            query = """SELECT id FROM 
-            (SELECT id FROM customers WHERE id >= %(n_first_id)d)
-             LIMIT %(n_customers)d""" % {'n_customers': n_customers, 'n_first_id': n_first_id}
-
-            ids = list(cursor.execute(query))
-            for i in range(0, len(ids)):
-                item = list(ids[i])
-                item.append(sumchar(item[0]))
-                ids[i] = tuple(item)
-
-            query = """
-                    DROP TABLE IF EXISTS temp;
-                    """
-            cursor.execute(query)
-
-            query = """
-                    CREATE TABLE temp (
-                    ID INTEGER UNIQUE NOT NULL,
-                    NumGroup INTEGER NOT NULL,
-                    PRIMARY KEY(ID))
-                    """
-            cursor.execute(query)
-
-            query = """
-                    INSERT INTO temp (ID, NumGroup)
-                    VALUES (?,?);
-                    """
-            cursor.executemany(query, ids)
-
-            query = """
-                    SELECT temp.NumGroup, COUNT(customers.ID)
-                    FROM customers
-                    LEFT JOIN temp
-                    ON customers.ID = temp.ID
-                    WHERE temp.NumGroup <> 'None'
-                    GROUP BY NumGroup
-                    """
-            out = tuple(cursor.execute(query))
-
-            query = """
-                   DROP TABLE IF EXISTS temp;
-                   """
-            cursor.execute(query)
-
-            return out
+    Лирическое отступление:
+    Была созданана тестовая БД. Не зная структуры основной бд, я мог не учесть некоторые особенности
+    например: мои ID это значения INTEGER. в то время как в настоящей базе они могли быть VARCHAR и тогда
+     ID 00001, 000001, 0000001 - это были бы три разных ID =), В то время как у меня все они равны 1.
+     Так же непонятно упорядочены ли записи по ID, нужно ли применять сортировку перед тем как достать записи из БД?
+     В общем все эти вопросы я бы задал перед тем как приступать к работе, но так как спращивать не у кого - работаем
+"""
 
 
-    print(func1(n_customers=2))
-    print(func2(n_first_id=11, n_customers=2))
+def func1(n_customers):
+    with sqlite3.connect('db/database.db') as db:
+        cursor = db.cursor()
+
+        db.create_function('sumchar_all', 1, sumchar)
+
+        query_values = {
+            'n_customers': n_customers
+        }
+
+        query = """
+                SELECT temp1.num_group, count(customers.id) FROM customers
+                LEFT JOIN (
+                    SELECT customers.id, sumchar_all(customers.id) AS num_group FROM (
+                        SELECT id FROM customers LIMIT :n_customers) AS customers) AS temp1
+                ON customers.id = temp1.id
+                WHERE num_group <> 'None'
+                GROUP BY num_group
+                """
+
+        return cursor.execute(query, {'n_customers': n_customers}).fetchall()
+
+
+def func2(n_first_id, n_customers):
+    with sqlite3.connect('db/database.db') as db:
+        cursor = db.cursor()
+
+        db.create_function('sumchar_all', 1, sumchar)
+
+        query_values = {
+            'n_first_id': n_first_id,
+            'n_customers': n_customers
+        }
+
+        query = """
+        SELECT temp1.num_group, count(customers.id) FROM customers
+        LEFT JOIN (
+            SELECT customers.id, sumchar_all(customers.id) as num_group FROM (
+                SELECT id FROM (SELECT id FROM customers WHERE id >= :n_first_id)
+            LIMIT :n_customers) as customers) AS temp1
+        ON customers.id = temp1.id
+        WHERE num_group <> 'None'
+        GROUP BY num_group
+        """
+
+        return cursor.execute(query, query_values).fetchall()
+
 
 if __name__ == '__main__':
-    main()
+    print(func1(6))
+    print(func2(120, 3))
